549a1989734c463d52b815e77a3a0085418b68d7
Humber-mv <mendozahumber1809@gmail.com>
2026-06-07 16:35:56 -0600

    Elaboración html y css de la sección de problemas

---
+++ js/home.js
@@ -0,0 +1,11 @@
+const circulo = document.querySelector('.circulo-progreso');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            circulo.classList.add('animado');
+        }
+    });
+}, { threshold: 0.5 });
+
+observer.observe(document.querySelector('.problems-stat'));
