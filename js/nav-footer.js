07690d78135d959e426bb5c45449e36ab6553c15
Humber-mv <mendozahumber1809@gmail.com>
2026-06-27 15:44:18 -0600

    Implementación de código html, css y js para el breadcrumb

--- js/nav-footer.js
+++ js/nav-footer.js
@@ -4,6 +4,19 @@ async function cargarNav() {
     const html = await res.text();
     document.getElementById('nav-container').innerHTML = html;
 
+    const pagina = window.location.pathname.split("/").pop();
+
+    const ubicacion = document.getElementById("current-location");
+
+    if (pagina === "mis-cultivos.html") {
+        ubicacion.textContent = "Mis cultivos";
+    }
+    else if (pagina === "registrar-cultivo.html") {
+        ubicacion.textContent = "Registrar cultivo";
+    }
+    else {
+        ubicacion.textContent = "Inicio";
+    }
     
 }
 
