7511daa9c2963768c67f5a8e24be8e9bec80ea56
Humber-mv <mendozahumber1809@gmail.com>
2026-06-29 20:00:09 -0600

    Implementación de las media queries

--- js/nav-footer.js
+++ js/nav-footer.js
@@ -17,6 +17,15 @@ async function cargarNav() {
         ubicacion.textContent = "Inicio";
     }
     
+    // Configuración del menú hamburguesa una vez que el HTML del navbar se ha insertado en el DOM
+    const boton = document.querySelector(".nav-toggle");
+    const links = document.querySelector(".nav-links");
+
+    if (boton && links) {
+        boton.addEventListener("click", () => {
+            links.classList.toggle("activo");
+        });
+    }
 }
 
 async function cargarFooter() {
